d7318318a46b43236f6ab6606797e51f27190220
C1904H <164913030+C1904H@users.noreply.github.com>
2024-09-23 11:06:04 +0100

    integrated atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('e5c2a72215a64d2bb64b4403e177a2df').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
